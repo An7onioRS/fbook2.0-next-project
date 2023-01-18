8234ee7176b4020223210293f667f4f1b96f20c2
Antonio Mansory <antoniosimeonov7@gmail.com>
2023-01-18 10:14:39 +0200

    Add style to the central side of the header

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,23 +1,33 @@
 import React from 'react'
 import facebookImg from '../assets/facebook1.png'
 import Image from 'next/image'
+import { MdHome } from "react-icons/md";
+import { FiPlayCircle, FiFlag, FiMessageCircle } from "react-icons/fi";
+import { BsCart3 } from "react-icons/bs";
+import { GrGroup, GrAppsRounded } from "react-icons/gr";
+import { FaBell } from "react-icons/fa";
+import { AiOutlineMessage } from "react-icons/ai";
 
 const Header = () => {
   return (
-    <div className='ml-2 p-4'>
+    <div className='flex p-4 items-center justify-between'>
         {/* Left side */}
         <div className='flex items-center'>
             <div className='w-10 h-12'>
                 <Image src={facebookImg}/>
             </div>
             <div className='ml-2'>
-                <input type="text" placeholder='Search Facebook' className='soutline-0 bg-[#f2f3f7] p-2 rounded-full pl-4'/>
+                <input type="text" placeholder='Search Facebook' className='outline-0 bg-[#f2f3f7] p-2 rounded-full pl-4'/>
             </div> 
         </div>
 
-
-
-        {/* Center */}
+        <div className='flex items-center space-x-7'>
+            <MdHome className='w-9 h-8'/>
+            <FiFlag className='w-7 h-8' />
+            <FiPlayCircle className='w-7 h-8' />
+            <BsCart3 className='w-7 h-8' />
+            <GrGroup className='w-7 h-8' />
+        </div>
         {/* Right side */}
     </div>
   )
