5f0ab506911a0212c983a9bf968b6a1a4fde5087
Antonio Mansory <antoniosimeonov7@gmail.com>
2023-01-18 13:28:23 +0200

    Complete header styles

--- components/Header.jsx
+++ components/Header.jsx
@@ -7,20 +7,22 @@ import { BsCart3 } from "react-icons/bs";
 import { GrGroup, GrAppsRounded } from "react-icons/gr";
 import { FaBell } from "react-icons/fa";
 import { AiOutlineMessage } from "react-icons/ai";
+import userImg from '../assets/guy7.jpg'
 
 const Header = () => {
   return (
-    <div className='flex p-4 items-center justify-between'>
-        {/* Left side */}
-        <div className='flex items-center'>
+    <div className='flex p-4 items-center justify-between border-b lg:px-10'>
+        {/* Left part */}
+        <div className='flex items-center mr-4'>
             <div className='w-10 h-12'>
-                <Image src={facebookImg}/>
+                <Image src={facebookImg} alt='facebook logo'/>
             </div>
             <div className='ml-2'>
-                <input type="text" placeholder='Search Facebook' className='outline-0 bg-[#f2f3f7] p-2 rounded-full pl-4'/>
+                <input type="text" placeholder='Search Facebook' className='hidden sm:block outline-0 bg-[#f2f3f7] rounded-full p-4'/>
             </div> 
         </div>
 
+        {/* Central part */}
         <div className='flex items-center space-x-7'>
             <MdHome className='w-9 h-8'/>
             <FiFlag className='w-7 h-8' />
@@ -28,7 +30,18 @@ const Header = () => {
             <BsCart3 className='w-7 h-8' />
             <GrGroup className='w-7 h-8' />
         </div>
-        {/* Right side */}
+
+        {/* Right part */}
+        <div className='flex items-center space-x-6 ml-4'>
+            <div className='hidden md:flex items-center space-x-6'>
+                <GrAppsRounded className='w-7 h-8' />
+                <FaBell className='w-7 h-8' />
+                <AiOutlineMessage className='w-7 h-8' />
+            </div>
+            <div className='sm:block w-10 h-10'>
+                <Image src={userImg} className='rounded-full' alt='user image'/>
+            </div>
+        </div>
     </div>
   )
 }
